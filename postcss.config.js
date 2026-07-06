d7d941d7f7b65b2097cecaa71e1395de7c4ecbb7
mahibulhasandev <mahibulhasandev@gmail.com>
2026-07-06 23:58:46 +0600

    add responsive login page

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//     Plugins: [
+//         require('tailwindcss')
+//         require('autoprefixer')
+//     ],
+// }
+
+module.exports = {
+    plugins: {
+        '@tailwindcss/postcss': {},
+    },
+};
